a884fc3e4994cb9d1906092eda3ae8fef64d8ba2
Tbelzbby <belloafeez28@gmail.com>
2024-05-08 10:31:32 +0100

    phaseGate

--- dayTwo/javascripts/MyFunctions.js
+++ dayTwo/javascripts/MyFunctions.js
@@ -1,9 +1,11 @@
 function evenIndex(numbers) {
-    var result = new Array(Math.floor(numbers.length / 2));
-    for (var index = 0; index < numbers.length; index += 2) {
-        result[index / 2] = numbers[index];
-    }
-    return result;
+    const evenElements = [];
+
+    for (let index = 0; index < numbers.length; index += 2) {
+        evenElements.push(numbers[index]); 
+}
+
+    return evenElements; 
 }
 
 module.exports = evenIndex;
