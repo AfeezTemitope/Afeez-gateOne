1a9e026d83dce112d8ceff066f0daa6e5ce22e6b
Tbelzbby <belloafeez28@gmail.com>
2024-05-15 09:44:22 +0100

    update

---
+++ day_seven/mentrual_app.py
@@ -0,0 +1,9 @@
+flowdate
+safe period
+ovulation date
+
+OVULATOR_OCCURENCE = 14
+first day of period = 1
+next date = random(21,35)days //noraml for adult women
+ovulation = next_date - 14
+safeperiod = flowdate - 18
